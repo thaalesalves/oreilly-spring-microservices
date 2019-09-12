e829685c18946790529ddfa8006fe8a9317e4b5c
Thales Alves <thales@thalesalv.es>
2019-09-12 18:34:41 -0300

    pom updated; removed configuration folder from project

--- microservices-course-config-client/src/main/java/es/thalesalv/MicroservicesCourseConfigClientApplication.java
+++ microservices-course-config-client/src/main/java/es/thalesalv/MicroservicesCourseConfigClientApplication.java
@@ -3,10 +3,12 @@ package es.thalesalv;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RefreshScope
 @RestController
 public class MicroservicesCourseConfigClientApplication {
 
