2740e153a66956dc5fcb6c5f68b4ee51fd9f8430
Thales Alves <thales@thalesalv.es>
2019-09-13 13:14:59 -0300

    first client set up

--- microservices-course-eureka-client/src/main/java/es/thalesalv/MicroservicesCourseEurekaClientApplication.java
+++ microservices-course-eureka-client/src/main/java/es/thalesalv/MicroservicesCourseEurekaClientApplication.java
@@ -2,8 +2,10 @@ package es.thalesalv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicesCourseEurekaClientApplication {
 
 	public static void main(String[] args) {
