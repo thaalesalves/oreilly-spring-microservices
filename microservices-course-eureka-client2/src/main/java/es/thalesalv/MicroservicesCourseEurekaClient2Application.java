6b0fa6bc6c9d05003b89570fb36bcec2a70866b6
Thales Alves <thales@thalesalv.es>
2019-09-13 13:31:21 -0300

    second client set up

--- microservices-course-eureka-client2/src/main/java/es/thalesalv/MicroservicesCourseEurekaClient2Application.java
+++ microservices-course-eureka-client2/src/main/java/es/thalesalv/MicroservicesCourseEurekaClient2Application.java
@@ -1,10 +1,28 @@
 package es.thalesalv;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class MicroservicesCourseEurekaClient2Application {
+
+	@Autowired
+	private EurekaClient client;
+
+	@RequestMapping("/serviceinfo")
+	public String serviceInfo() {
+		InstanceInfo instance = client.getNextServerFromEureka("myClient", false);
+		return instance.getHomePageUrl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesCourseEurekaClient2Application.class, args);
