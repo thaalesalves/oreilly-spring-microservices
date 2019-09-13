6f7db9454abce07c9377de18295ab561a0cbfae9
Thales Alves <thales@thalesalv.es>
2019-09-13 15:33:36 -0300

    transformed into rest controller

--- microservices-course-simple-service-2/src/main/java/es/thalesalv/MicroservicesCourseSimpleService2Application.java
+++ microservices-course-simple-service-2/src/main/java/es/thalesalv/MicroservicesCourseSimpleService2Application.java
@@ -1,11 +1,28 @@
 package es.thalesalv;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MicroservicesCourseSimpleService2Application {
 
+	@Value("${server.port}")
+	public String port;
+	
+	@RequestMapping("/execute")
+	public String execute() {
+		return "Hello from " + this.port;
+	}
+	
+	@RequestMapping("/")
+	public String status() {
+		return "Up!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesCourseSimpleService2Application.class, args);
 	}
