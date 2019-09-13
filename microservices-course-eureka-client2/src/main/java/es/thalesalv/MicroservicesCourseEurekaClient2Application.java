da734bf0c54fe0153c13f5cc315557676ba6fea5
Thales Alves <thales@thalesalv.es>
2019-09-13 13:15:07 -0300

    created second client

---
+++ microservices-course-eureka-client2/src/main/java/es/thalesalv/MicroservicesCourseEurekaClient2Application.java
@@ -0,0 +1,13 @@
+package es.thalesalv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCourseEurekaClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCourseEurekaClient2Application.class, args);
+	}
+
+}
