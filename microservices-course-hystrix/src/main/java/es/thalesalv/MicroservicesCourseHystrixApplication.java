950e3f51deab1a4e6479446c6bc09b49865735bf
Thales Alves <thales@thalesalv.es>
2019-09-13 15:16:21 -0300

    circuit breaking

--- microservices-course-hystrix/src/main/java/es/thalesalv/MicroservicesCourseHystrixApplication.java
+++ microservices-course-hystrix/src/main/java/es/thalesalv/MicroservicesCourseHystrixApplication.java
@@ -6,17 +6,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @SpringBootApplication
 @RestController
-@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class MicroservicesCourseHystrixApplication {
 
 	@Autowired
@@ -27,13 +31,16 @@ public class MicroservicesCourseHystrixApplication {
 		return new RestTemplate();
 	}
 
+	@SuppressWarnings("unchecked")
 	@RequestMapping("/startclient")
-	@HystrixCommand(fallbackMethod = "failOver")
-	public List<String> startClient() {
+	@HystrixCommand(fallbackMethod = "failOver", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500") })
+	public List<String> startClient(@RequestParam long time) throws InterruptedException {
+		Thread.sleep(time);
 		return this.restTemplate.getForObject("http://localhost:8888/service", List.class);
 	}
 
-	public List<String> failOver() {
+	public List<String> failOver(long time) {
 		return Arrays.asList("default1", "default2");
 	}
 
