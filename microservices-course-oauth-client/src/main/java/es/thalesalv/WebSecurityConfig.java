904a8acd748ea722c63d76b4f6c73cbd6b56b0e5
Thales Alves <thales@thalesalv.es>
2019-09-16 10:40:57 -0300

    oauth authorization and access

--- microservices-course-oauth-client/src/main/java/es/thalesalv/WebSecurityConfig.java
+++ microservices-course-oauth-client/src/main/java/es/thalesalv/WebSecurityConfig.java
@@ -2,24 +2,30 @@ package es.thalesalv;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	// @Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+//	@Bean
+//	// @Override
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user1").password("password1").roles("USER")
-		.and().withUser("user2").password("password2").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("user1").password("password1").roles("USER").and().withUser("user2")
+				.password("password2").roles("ADMIN");
+		auth.eraseCredentials(false);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
 	}
 
 	@SuppressWarnings("deprecation")
--- microservices-course-oauth-server/src/main/java/es/thalesalv/MicroservicesCourseOauthServerApplication.java
+++ microservices-course-oauth-server/src/main/java/es/thalesalv/MicroservicesCourseOauthServerApplication.java
@@ -2,11 +2,25 @@ package es.thalesalv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@RestController
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MicroservicesCourseOauthServerApplication {
+
+	@RequestMapping("/resource/endpoint")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String endpoint() {
+		return "This message is protected by the resource server.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesCourseOauthServerApplication.class, args);
